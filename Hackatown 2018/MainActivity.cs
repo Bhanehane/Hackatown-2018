67f0715d0747f19c0af17b0ca56d8fc71e16c7e5
Frédéric Fortin <f.fortin1997@gmail.com>
2018-01-21 04:26:19 -0500

    Maps activity

--- Hackatown 2018/MainActivity.cs
+++ Hackatown 2018/MainActivity.cs
@@ -1,6 +1,20 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Net;
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Gms.Location;
+using System.IO;
+using System.Globalization;
+using Android.Content;
+using Android.Views.InputMethods;
+
+
+
 
 namespace Hackatown_2018
 {
@@ -10,11 +24,25 @@ namespace Hackatown_2018
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Set our view from the "main" layout resource
+
+            Button butt = FindViewById<Button>(Resource.Id.omaewa);
+
+
+            butt.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(Maps));
+
+                StartActivityForResult(intent, 1);
+            };
+
+
 
         }
+
+
 
 
     }
