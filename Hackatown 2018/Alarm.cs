a465bee31fd51aed08b9a22007d1d1a477d0e75e
antoine lamontagne <antoine.lamontagne@hotmail.ca>
2018-01-21 04:30:34 -0500

    alarm

--- Hackatown 2018/Alarm.cs
+++ Hackatown 2018/Alarm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Globalization;
 
 using Android.App;
 using Android.Content;
@@ -10,7 +11,7 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
-using Hackatown_2018.Broadcast;
+using Android.Media;
 
 namespace Hackatown_2018
 {
@@ -19,27 +20,38 @@ namespace Hackatown_2018
         public Context Context { get; private set; }
         private AlarmManager Manager { get; set; }
         private Intent CurrentIntent { get; set; }
+        private PendingIntent PendingIntent { get; set; }
         public string Time { get; set; }
         public string DayOfWeek { get; set; }
         public string DesiredTimeArrival { get; set; }
+        public DateTime DateTime { get; set; }
+
         public Alarm(Context context)
         {
             Context = context;
             Manager = Context.GetSystemService(Context.AlarmService) as AlarmManager;
+            DateTime = new DateTime(2018, 1, 21, 4, 26, 5);
         }
 
-        public void StartAlarm(bool isNotif, bool isRepeat)
+        public void StartAlarm()
         {
-            PendingIntent pending;
-            CurrentIntent = new Intent(Context, typeof(AlarmReceiver));
-            pending = PendingIntent.GetBroadcast(Context, 0, CurrentIntent, 0);
+            long interval = GetMilliSec(DateTime);
+            if (interval > 0)
+            {
+                CurrentIntent = new Intent(Context, typeof(AlarmReceiver));
+                string message = "On se réveille";
+                CurrentIntent.PutExtra("message", message);
+                PendingIntent = PendingIntent.GetBroadcast(Context, 0, CurrentIntent, 0);
 
-            Manager.Set(AlarmType.RtcWakeup, SystemClock.ElapsedRealtime(), pending);
+                Manager.Set(AlarmType.ElapsedRealtimeWakeup, SystemClock.ElapsedRealtime() + interval, PendingIntent);
+            }
+
         }
 
-        public void StopAlarm()
+        public long GetMilliSec(DateTime time)
         {
-
+            TimeSpan interval = time - DateTime.Now;
+            return Convert.ToInt64(interval.TotalMilliseconds);
         }
 
     }
---
+++ Hackatown 2018/AlarmReceiver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Media;
+
+namespace Hackatown_2018
+{
+    [BroadcastReceiver(Enabled = true)]
+    public class AlarmReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Toast.MakeText(context, "This is my alarm", ToastLength.Short).Show();
+            string message = intent.GetStringExtra("message");
+
+            Notification.Builder builder = new Notification.Builder(context).
+                SetAutoCancel(true).
+                SetSound(RingtoneManager.GetDefaultUri(RingtoneType.Alarm), Stream.Alarm).
+                SetContentTitle("Alarm").
+                SetContentText(message).
+                SetVibrate(new long[] { 0, 500, 300, 500, 300 }).
+                SetPriority(4).
+                SetSmallIcon(Resource.Drawable.ic_notif);
+
+            NotificationManager notificationManager = context.GetSystemService(Context.NotificationService) as NotificationManager;
+            Notification build = builder.Build();
+            notificationManager.Notify(0, build);
+        }
+    }
+}
