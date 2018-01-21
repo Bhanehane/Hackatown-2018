d9e8aa3411565926d58a113a13d5c588ac6361b4
Frédéric Fortin <f.fortin1997@gmail.com>
2018-01-21 06:13:10 -0500

    Développement du calcul du temps de voyage

--- Hackatown 2018/Alarm.cs
+++ Hackatown 2018/Alarm.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Globalization;
-
+using Newtonsoft.Json;
+using System.Net;
 using Android.App;
 using Android.Content;
 using Android.OS;
@@ -12,6 +13,8 @@ using Android.Util;
 using Android.Views;
 using Android.Widget;
 using Android.Media;
+using System.IO;
+
 
 namespace Hackatown_2018
 {
@@ -58,6 +61,92 @@ namespace Hackatown_2018
         public void CalculateAlarmTime(DateTime preparationTime, DateTime desiredTimeArrival)
         {
             AlarmTime = desiredTimeArrival - preparationTime;
+        }
+
+
+        public void CalculateTravelTimeWithTraffic()
+        {
+            int timeNeeded = GetTimeFromAPI(Position[0], Position[1], Destination[0], Destination[1]);
+            
+        }
+
+        public long ConvertToTimestamp(DateTime value)
+        {
+            long epoch = (value.Ticks - 621355968000000000) / 10000000;
+            return epoch - 7200;
+        }
+
+        private int[] GetTimeFromAPI(double lat1, double long1, double lat2, double long2, DateTime date)
+        {
+            string html = string.Empty;
+            string[] sortie1;
+            string[] sortie2;
+            int[] resultat = new int[2];
+            long timeStamp = ConvertToTimestamp(date);
+            string url = @"https://maps.googleapis.com/maps/api/directions/json?origin=" + lat1.ToString() + ',' + long1.ToString() + "&destination=" + lat2.ToString() + ',' + long2.ToString() + "&departure_time=" + timeStamp.ToString() + "&key=AIzaSyDwgUfhlc26aVBOm2NZUAA5vQHixHIvyp0";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (System.IO.Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+            dynamic jsonResponse = JsonConvert.DeserializeObject(html);
+            string txt = jsonResponse.routes[0].legs[0].duration.text;
+            string txt2 = jsonResponse.routes[0].legs[0].duration_in_traffic.text;
+            sortie1 = txt.Split(' ');
+            sortie2 = txt2.Split(' ');
+            if (sortie1[1][0] == 'h')
+            {
+                resultat[0] = int.Parse(sortie1[0]) * 60;
+            }
+            else
+            {
+                resultat[0] = int.Parse(sortie1[0]);
+            }
+            if (sortie2[1][0] == 'h')
+            {
+                resultat[1] = int.Parse(sortie2[0]) * 60;
+            }
+            else
+            {
+                resultat[1] = int.Parse(sortie2[0]);
+            }
+            return resultat;
+        }
+
+        private int GetTimeFromAPI(double lat1, double long1, double lat2, double long2)
+        {
+            string html = string.Empty;
+            string[] sortie1;
+            int resultat = 0;
+            string url = @"https://maps.googleapis.com/maps/api/directions/json?origin=" +
+                lat1.ToString("G", new CultureInfo("en-US")) + ',' + long1.ToString("G", new CultureInfo("en-US")) +
+                "&destination=" + lat2.ToString("G", new CultureInfo("en-US")) + ',' + long2.ToString("G", new CultureInfo("en-US")) + "&key=AIzaSyCevyyL4bEYdWVoaH9fCecYk9JEfQtblo8";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (System.IO.Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+            dynamic jsonResponse = JsonConvert.DeserializeObject(html);
+            string txt = jsonResponse.routes[0].legs[0].duration.text;
+            sortie1 = txt.Split(' ');
+            if (sortie1[1][0] == 'h')
+            {
+                resultat = int.Parse(sortie1[0]) * 60;
+            }
+            else
+            {
+                resultat = int.Parse(sortie1[0]);
+            }
+            return resultat;
+
         }
     }
 }
